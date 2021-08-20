170ca8a05ab4b409e74edcddf91ef5838d34b68e
martinpvz <martin.paniagua200013@gmail.com>
2021-08-20 18:49:59 -0500

    se agrega una datalist para los cupones

--- descuentos.js
+++ descuentos.js
@@ -21,9 +21,17 @@
 function calcularPrecioConDescuento() {
   let precio = document.getElementById("InputProducto").value;
   let descuento = document.getElementById("InputDescuento").value;
-
-  let precioDescuento = (precio * (100-descuento))/100;
+  let cupon = document.getElementById("InputCupon").value;
 
   let parrafo = document.getElementById("ResultPrice");
-  parrafo.innerText = "El precio con descuento es $" + precioDescuento;
+  let precioDescuento = (precio * (100-descuento))/100;
+
+  if ( cupon == "FASHIONWEEK" ) {
+    parrafo.innerText = "El precio con descuento es $" + precioDescuento + ". Menos tu cupón " + cupon + " quedaría en: $" + ((precioDescuento*(100-15))/100);
+  } else if ( cupon == "VERANO2021") {
+    parrafo.innerText = "El precio con descuento es $" + precioDescuento + ". Menos tu cupón " + cupon + " quedaría en: $" + ((precioDescuento*(100-10))/100);
+  } else {
+    parrafo.innerText = "El precio con descuento es $" + precioDescuento;
+  }
+  console.log(cupon);
 }
